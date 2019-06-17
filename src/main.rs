bca28f77ec0a5584bc0aaec80362ab79d2565d4e
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-06-17 14:35:49 -0400

    storing word count in a hash map. Had to create a new String from the string slice in order for the hash map to have ownership of the key

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
 
 fn main() {
     let filename = "src/THEGODFATHER.dat";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    for (index, line) in reader.lines().enumerate() {
+    let mut words = HashMap::new();
+
+    for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         for word in line.split_whitespace() {
-            println!("{}", word);
+            let key = String::from(word);
+            let count = words.entry(key).or_insert(0);
+            *count += 1;
         }
     }
+
+    println!("{:?}", words);
 }
