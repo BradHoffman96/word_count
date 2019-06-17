adf48354e8e5c0e87d4cab372890d137754f6bff
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-06-17 14:22:51 -0400

    reading the contents of the file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "src/THEGODFATHER.dat";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        println!("{}. {}", index + 1, line);
+    }
 }
